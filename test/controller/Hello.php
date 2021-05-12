96d5f4e965ccd1015cb1919eb89db174c8b6bc16
KyoukoLi <76253308+KyoukoLi@users.noreply.github.com>
2021-05-12 21:12:50 +0800

    Add files via upload

---
+++ test/controller/Hello.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\test\controller;
+
+class Hello
+{
+    public function luck()
+    {
+        return '1';
+    }
+    public function luck1()
+    {
+        return '12';
+    }
+}
